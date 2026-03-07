87976be364db402a00116bda0db387235cb489ec
Ivancin_01 <lopezdimanuel@gmail.com>
2026-03-07 12:26:11 +0100

    No explotes

---
+++ gestion-practicas/src/main/java/com/tfg/gestion_practicas/GestionPracticasApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.gestion_practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPracticasApplication.class, args);
+	}
+
+}
